3ac621ec8c0f5a6271b7c5dacff4934b40194cd9
MironMarins <rm551801@fiap.com.br>
2023-10-04 20:55:59 -0300

    transformação de json

--- geradorDeJSON/jsonParaDic.py
+++ geradorDeJSON/jsonParaDic.py
@@ -6,3 +6,12 @@ with open('geradorDeJSON\SP.json') as Cidades_SaoPaul:
 print(dados[features])'''
 
 https://github.com/chandez/Estados-Cidades-IBGE/commit/4cb009e429ad4174c46c425c3f5c05c596d244db
+
+#pegar nome de estado
+
+import json
+
+with open("json\estados.json") as meu_json:
+    dados = json.load(meu_json)
+
+print(dados['data'][2]['Nome'])
