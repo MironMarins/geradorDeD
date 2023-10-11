213e5da3b18cbc9afe33a44d5322e8d21e0f396d
MironMarins <rm551801@fiap.com.br>
2023-10-11 19:35:27 -0300

    faltam duas tabelas

---
+++ estado/T_RHSTU_TELEFONE_PACIENTE copy.py
@@ -0,0 +1,48 @@
+import oracledb
+import aleatorio
+import aleatorioEstado
+EmailsPaciente=[]
+repeticao = 2
+
+def T_RHSTU_PACIENTE_EMAIL_PACIENTE(id_paciente,id_email,ds_email,tp_email,st_email,dt_cadastro,nm_usuario):
+    emailPaciente = {}
+    emailPaciente['ID_PACIENTE'] = id_paciente #F
+    emailPaciente['ID_TELEFONE'] = id_email #P
+    emailPaciente['DS_EMAIL'] = ds_email
+    emailPaciente['TP_EMAIL'] = tp_email
+    emailPaciente['ST_EMAIL'] = st_email
+    emailPaciente['DT_CADASTRO'] = dt_cadastro
+    emailPaciente['NM_USUARIO'] = nm_usuario
+    return emailPaciente 
+
+for i in range(repeticao):
+            idPaciente = #F
+            idEmail = #P
+            dsEmail =
+            tpEmail =
+            stEmail =
+            dtCadastro = 
+            nmUsuario =
+            
+
+
+
+            EmailPaciente = T_RHSTU_PACIENTE_EMAIL_PACIENTE(id_paciente=,id_email=,ds_email=,tp_email=,st_email=,dt_cadastro=dataCadastro,nm_usuario=usuarioNomeCompleto)
+            EmailsPaciente.append(EmailPaciente)
+            
+
+
+try:
+    with oracledb.connect(user='rm551801',password='040591',dsn="oracle.fiap.com.br/orcl") as conexao:
+        with conexao.cursor() as cur:
+             ins = '''INSERT INTO T_RHSTU_ESTADO(ID_ESTADO,SG_ESTADO,NM_ESTADO,DT_CADASTRO,NM_USUARIO) 
+             VALUES(:ID_ESTADO,:SG_ESTADO,:NM_ESTADO,TO_DATE(:DT_CADASTRO,'YYYY-MM-DD'),:NM_USUARIO)'''        
+             for info in listaEstados:
+                    cur.execute(ins, info)
+                    conexao.commit()
+                    
+            
+            
+except oracledb.DatabaseError as erro:
+                print("deu erro ESTADO!")
+                print(erro)
---
+++ estado/T_RHSTU_TELEFONE_PACIENTE.py
@@ -0,0 +1,52 @@
+import oracledb
+import aleatorio
+import aleatorioEstado
+TelefonesPaciente=[]
+repeticao = 2
+
+def T_RHSTU_PACIENTE_TELEFONE_PACIENTE(id_paciente,id_telefone,nr_ddi,nr_ddd,nr_telefone,tp_telepone,st_telefone,dt_cadastro,nm_usuario):
+    telefonePaciente = {}
+    telefonePaciente['ID_PACIENTE'] = id_paciente #PF
+    telefonePaciente['ID_TELEFONE'] = id_telefone #P
+    telefonePaciente['NR_DDI'] = nr_ddi 
+    telefonePaciente['NR_DDD'] = nr_ddd
+    telefonePaciente['NR_TELEFONE'] = nr_telefone
+    telefonePaciente['TP_TELEFONE'] = tp_telepone
+    telefonePaciente['ST_TELEFONE'] = st_telefone
+    telefonePaciente['DT_CADASTRO'] = dt_cadastro
+    telefonePaciente['NM_USUARIO'] = nm_usuario
+    return telefonePaciente 
+
+for i in range(repeticao):
+            idPaciente = #PF
+            idTelefone = #P
+            nrDDI =
+            nrDDD =
+            nrTelefone =
+            tpTelepone =
+            stTelefone =
+            dtCadastro =
+            nmUsuario =
+            
+
+
+
+            TelefonePaciente = T_RHSTU_PACIENTE_TELEFONE_PACIENTE(id_paciente=,id_telefone=,nr_ddi=,nr_ddd=,nr_telefone=,tp_telepone=,st_telefone=,dt_cadastro=dataCadastro,nm_usuario=usuarioNomeCompleto)
+            TelefonesPaciente.append(TelefonePaciente)
+            
+
+
+try:
+    with oracledb.connect(user='rm551801',password='040591',dsn="oracle.fiap.com.br/orcl") as conexao:
+        with conexao.cursor() as cur:
+             ins = '''INSERT INTO T_RHSTU_ESTADO(ID_ESTADO,SG_ESTADO,NM_ESTADO,DT_CADASTRO,NM_USUARIO) 
+             VALUES(:ID_ESTADO,:SG_ESTADO,:NM_ESTADO,TO_DATE(:DT_CADASTRO,'YYYY-MM-DD'),:NM_USUARIO)'''        
+             for info in listaEstados:
+                    cur.execute(ins, info)
+                    conexao.commit()
+                    
+            
+            
+except oracledb.DatabaseError as erro:
+                print("deu erro ESTADO!")
+                print(erro)
