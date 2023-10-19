9fc6622f497cfeb55315fc0e3daf339e07836c81
MironMarins <rm551801@fiap.com.br>
2023-10-19 07:29:06 -0300

    inplementado algoritmo de  hierarquia

--- secundarios/T_RHSTU_FUNCIONARIO/ierarquiaHospitalar.py
+++ secundarios/T_RHSTU_FUNCIONARIO/ierarquiaHospitalar.py
@@ -1,18 +1,13 @@
 import aleatorioFuncionario
 
-#presidente = 27
-#diretor = 27*3 = 81
-#gerente = 81*5 = 405
-#coordenador =  7*405 = 2835
-#funcionario =  2835 * 15 = 42525 medicos
-#funcionario =  8 * 27 = 216 motoristas
+
 
 hierarquiaHosp = []
-listaPresidente =[]
-listaDiretor =[]
-listaGerente=[]
-listaCoodenador=[]
-listafunc=[]
+
+
+
+
+
 
 def presiDic(numero,nome,subalterno):
     presidente ={}
@@ -21,6 +16,7 @@ def presiDic(numero,nome,subalterno):
     presidente['cargo'] = "presidente"
     presidente['subalterno'] = subalterno
     return presidente
+#listaPresidente.append(presiDic(1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),))
 def dretorDic(numero,nome,subalterno):
     diretor ={}
     diretor['numero'] = numero
@@ -42,31 +38,114 @@ def CoordenadorDic(numero,nome,subalterno):
     coordenador['nome']= nome
     coordenador['cargo'] = "coordenador"
     coordenador['subalterno'] = subalterno
-    return coordenador
 
-def funcionarioDic(numero,nome,subalterno):
+    return coordenador
+#listaCoodenador.append(CoordenadorDic(1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),"medico"))
+def funcionarioDic(numero,nome,cargo):
+
     funcionario ={}
     funcionario['numero'] = numero
     funcionario['nome']= nome
-    funcionario['cargo']="funcionario"
-    funcionario['subalterno'] = subalterno
+    funcionario['cargo']=cargo
+    
     return funcionario
+
 c=0
 print(c)
-for p in range(1,28):
-    hierarquiaHosp.append(presiDic(p,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
-    for d in range(0,2):
-        hierarquiaHosp.append(dretorDic(d,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
-        for g in range(0,2):
-            hierarquiaHosp.append(gerenteDic(g,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
-            for c in range(0,2):
-                hierarquiaHosp.append(CoordenadorDic(c,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
-                for f in range(0,2):
-                    hierarquiaHosp.append(funcionarioDic(f,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
-                for f in range(3,5):
-                    hierarquiaHosp.append(funcionarioDic(f,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
-                
-                    
+#for p in range(1,28):
+    #hierarquiaHosp.append(presiDic(p,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
+    #for d in range(0,2):
+        #hierarquiaHosp.append(dretorDic(d,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
+        #for g in range(0,2):
+            #hierarquiaHosp.append(gerenteDic(g,aleatorioFuncionario.nomes(),aleatorioFuncionario.nomes()))
+            
 
-print(hierarquiaHosp)
 
+#for f in range(1,3):
+    #listafunc.append(funcionarioDic(f,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),"motorista"))
+#primeira lista de funcionarios
+
+listafunc=[]
+for f in range(0,18):
+        listafunc.append(funcionarioDic(f+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),"medico"))
+#primeira lista coordenadores 
+listaCoodenador=[]
+
+for k in range(0,7):
+    listaCoodenador.append(CoordenadorDic(k+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listafunc)) 
+    listafunc=[]
+    for f in range(0,18):
+        listafunc.append(funcionarioDic(f+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),"medico"))
+#primeira lista de gerente
+listaGerente=[]
+for g in range(0,5):
+     listaGerente.append(gerenteDic(g+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listaCoodenador))
+     listaCoodenador=[]
+     for k2 in range(0,7):
+        listaCoodenador.append(CoordenadorDic(k2+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listafunc))
+        listafunc=[]
+        for f in range(0,18):
+            listafunc.append(funcionarioDic(f+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),"medico"))  
+#primeira lista de diretores
+listaDiretor =[]
+for d in range(0,3):
+    listaDiretor.append(dretorDic(d+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listaGerente))
+    listaGerente=[]
+    for g in range(0,5):
+        listaGerente.append(gerenteDic(g+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listaCoodenador))
+        listaCoodenador=[]
+        for k2 in range(0,7):
+            listaCoodenador.append(CoordenadorDic(k2+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listafunc))
+            listafunc=[]
+            for f in range(0,18):
+                listafunc.append(funcionarioDic(f+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),"medico"))
+#primeira lista de presidentes
+listaPresidente=[]
+for p in range(0,27):
+    listaPresidente.append(presiDic(p+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listaDiretor))
+    listaDiretor=[]
+    for d in range(0,3):
+        listaDiretor.append(dretorDic(d+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listaGerente))
+        listaGerente=[]
+        for g in range(0,5):
+            listaGerente.append(gerenteDic(g+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listaCoodenador))
+            listaCoodenador=[]
+            for k2 in range(0,7):
+                listaCoodenador.append(CoordenadorDic(k2+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),listafunc))
+                listafunc=[]
+                for f in range(0,18):
+                    listafunc.append(funcionarioDic(f+1,aleatorioFuncionario.nomes()+ " "+ aleatorioFuncionario.sobrenomes(),"medico"))
+
+
+#presidente = 27
+#diretor = 27*3 = 81
+#gerente = 81*5 = 405
+#coordenador =  7*405 = 2835
+#funcionario =  2835 * 18 = 51030 funcionarios
+                 
+#print("=-"*10)
+#print(listafunc)
+#print("=-"*10)
+#print(listaCoodenador)
+#print("=-"*10)
+#print(listaGerente)
+#print("=-"*10)
+#print(listaDiretor)
+#print("=-"*10)
+#print(listaPresidente)
+#print("-="*20, "informacao")
+print(len(listaPresidente))
+
+resultDiretor=len(listaPresidente[0]['subalterno'])
+novalistaDiretor = listaPresidente[0]['subalterno']
+print(resultDiretor)
+resultgerente=len(novalistaDiretor[0]['subalterno'])
+novalistaGerente=novalistaDiretor[0]['subalterno']
+print(resultgerente)
+resultCoordendor=len(novalistaGerente[0]['subalterno'])
+novalistaCoordenador=novalistaGerente[0]['subalterno']
+print(resultCoordendor)
+resultFunc = len(novalistaCoordenador[0]['subalterno'])
+novalistaFunc = novalistaCoordenador[0]['subalterno']
+print(resultFunc)
+#print(novalistaDiretor)
